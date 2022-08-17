fc4e1630f21b70fdd6dc1b54f5ea167070fb7898
carlton <carltonmbaja0@gmail.com>
2022-08-17 12:34:51 +0300

    commit tree 05f64c2a3f782fda2600ca0e25c073818ab376dc

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,24 @@
 class Dog
+    #setter method
+    def name=(name)
+        @name = name
+    end
 
+    # getter method
+    def name 
+        @name
+    end
+
+    # Initializing all together on this one
+    attr_accessor :breed
+
+    def breed=(breed)
+        @breed  = breed
+    end
+
+    # def breed
+    #     @breed
+    # end
 end
+kanye = Dog.new
+kanye.name = "Kanye"
