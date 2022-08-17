c48ada3bb3ed1a2751981c4d582ace4c6b5686d4
carlton <carltonmbaja0@gmail.com>
2022-08-17 12:35:07 +0300

    Complete

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
 class Person
+    def name=(name)
+        @name = name
+    end
 
+    def name
+        @name
+    end
+
+    # Initializing all together on this one
+
+    attr_accessor :name
+    attr_accessor :job
+
+    def job=(job)
+        @job = job
+    end
+    
 end
+
